56026cd4aa9f3d695a4af50cdd874cba668d2bdd
li-zhan-zhan <lzzvia@126.com>
2020-12-06 17:05:25 +0800

    修复显示效果

--- js/demo.js
+++ js/demo.js
@@ -1,13 +1,16 @@
 function $(name){return document.getElementById(name);}
 function tag(tagName){return document.getElementsByTagName(tagName);}
+function c(cname) {return document.getElementsByClassName(cname);}
 function start(){
 	getWeeks();
 	getCountdwon();
+	replaceStr();
 }
 function getWeeks(){
     var now = new Date();
-    var day = now.getDay();
-    if(day<6){
+    var day = now.getDay(); // 0：周日、1：周一
+	// console.log(now.getDay());
+    if(day<6 && day >0){
         var tds = tag("td");
         for(var i=day; i<tds.length; i+=7){
             tds[i].style.color = "red"
@@ -21,10 +24,7 @@ function getWeeks(){
 	var weeks = Math.ceil(weeksTemp);
 	$("week").innerHTML =weeks;
 }
-function changeOption(){
-    var index = $("mySelect").selectedIndex;
-    $("show").src=$("mySelect").options[index].value;
-}
+
 function getCountdwon(){
 	var message=$("message");
 	var CET4Date = new Date(2020,11,12); //2020年12月考试时间为2020年12月12日
@@ -39,4 +39,10 @@ function getCountdwon(){
 }
 function compteCountdown(endDate){
 	return Math.floor((endDate.getTime()-new Date().getTime())/(1000*60*60*24));
+}
+
+function replaceStr(){
+	var dels = c("del");
+	for(var i=0; i<dels.length; i++)
+		dels[i].innerHTML = "本课程已结课"
 }
